f28ccb340ad423e1ac2b9ea241d9430e7788335c
Wasabi <mview@hotmail.com>
2016-10-11 11:42:36 +0700

    Start POS 1

--- app/src/main/java/com/example/posstrsoftware/posstrsoftware/fragment/MainFragment.java
+++ app/src/main/java/com/example/posstrsoftware/posstrsoftware/fragment/MainFragment.java
@@ -1,20 +1,28 @@
 package com.example.posstrsoftware.posstrsoftware.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.posstrsoftware.posstrsoftware.R;
 import com.gc.materialdesign.views.ButtonRectangle;
+import com.inthecheesefactory.thecheeselibrary.view.SlidingTabLayout;
 
 
 /**
  * Created by nuuneoi on 11/16/2014.
  */
 public class MainFragment extends Fragment {
-    ButtonRectangle btn_Sale;
+    ViewPager viewPager;
+    SlidingTabLayout slidingTabLayout;
 
     public MainFragment() {
         super();
@@ -37,8 +45,50 @@ public class MainFragment extends Fragment {
 
     private void initInstances(View rootView) {
         // Init 'View' instance(s) with rootView.findViewById here
-        btn_Sale = (ButtonRectangle)rootView.findViewById(R.id.btn_Sale);
-        btn_Sale.setRippleSpeed(50);
+        viewPager = (ViewPager) rootView.findViewById(R.id.viewPager);
+        viewPager.setAdapter(new FragmentStatePagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return SaleMainFragment.newInstance();
+                    case 1:
+                        return ProductSetMainFragment.newInstance();
+                    case 2:
+                        return ContactMainFragment.newInstance();
+                    default:
+                        return null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 3;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                switch (position){
+                    case 0:
+                        return "           Sale Product         ";
+                    case 1:
+                        return "         Product Setting       ";
+                    case 2:
+                        return "             Contact             ";
+                    default:
+                        return "";
+                }
+            }
+        });
+
+        slidingTabLayout = (SlidingTabLayout) rootView.findViewById(R.id.slidingTabLayout);
+        slidingTabLayout.setViewPager(viewPager);
+
+        slidingTabLayout.setSelectedIndicatorColors(ContextCompat.getColor(getActivity(),R.color.colorOrange));
+
+
+
+
 
     }
 
---
+++ app/src/main/java/com/example/posstrsoftware/posstrsoftware/fragment/ProductSetMainFragment.java
@@ -0,0 +1,69 @@
+package com.example.posstrsoftware.posstrsoftware.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.posstrsoftware.posstrsoftware.R;
+
+
+/**
+ * Created by nuuneoi on 11/16/2014.
+ */
+public class ProductSetMainFragment extends Fragment {
+
+    public ProductSetMainFragment() {
+        super();
+    }
+
+    public static ProductSetMainFragment newInstance() {
+        ProductSetMainFragment fragment = new ProductSetMainFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main_productset, container, false);
+        initInstances(rootView);
+        return rootView;
+    }
+
+    private void initInstances(View rootView) {
+        // Init 'View' instance(s) with rootView.findViewById here
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    /*
+     * Save Instance State Here
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save Instance State here
+    }
+
+    /*
+     * Restore Instance State Here
+     */
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null) {
+            // Restore Instance State here
+        }
+    }
+}
