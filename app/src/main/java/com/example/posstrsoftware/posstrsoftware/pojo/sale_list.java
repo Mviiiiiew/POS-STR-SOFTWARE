1a5b33f9a79d665f987a619d98d33214eb603864
Wasabi <mview@hotmail.com>
2016-11-01 09:57:30 +0700

    Start POS 25

--- app/src/main/java/com/example/posstrsoftware/posstrsoftware/pojo/sale_list.java
+++ app/src/main/java/com/example/posstrsoftware/posstrsoftware/pojo/sale_list.java
@@ -9,13 +9,7 @@ public class sale_list {
     private String product_price;
     private String product_qrt;
 
-    public String getProduct_qrt() {
-        return product_qrt;
-    }
-
-    public void setProduct_qrt(String product_qrt) {
-        this.product_qrt = product_qrt;
-    }
+  
 
     public String getProduct_name() {
         return product_name;
