fca8b3f76ab04535dfb55fcabc0866af3ac069f1
Wasabi <mview@hotmail.com>
2017-05-04 15:58:23 +0700

    PosSTR

--- app/src/main/java/com/example/posstrsoftware/posstrsoftware/dao/GroupDAO.java
+++ app/src/main/java/com/example/posstrsoftware/posstrsoftware/dao/GroupDAO.java
@@ -14,7 +14,7 @@ import com.example.posstrsoftware.posstrsoftware.model.GroupList;
 import java.util.ArrayList;
 
 /**
- * Created by Wasabi on 10/17/2016.
+ * Created by Wasabi on 10/17/2016.ewerwerwerewrwer
  */
 
 public class GroupDAO {
